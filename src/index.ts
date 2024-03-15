2264808fdbaf6b6a0113961727a6dfbe3d4d065a
bwesterg <benton.westergaard@gmail.com>
2024-03-15 16:26:22 -0600

    saves progress

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,19 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from './routes/MyUserRoute'
+import myUserRoute from './routes/MyUserRoute';
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from './routes/MyRestaurantRoute';
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to mongo database"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 const app = express();
 app.use(express.json());
@@ -22,6 +30,7 @@ app.get("/test", async (req: Request, res: Response) => {
 
 // /api/my/user
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -21,3 +21,28 @@ export const validateMyUserRequest = [
   body("country").isString().notEmpty().withMessage("Country should be a string"),
   handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant name is mandatory"),
+  body("city").notEmpty().withMessage("City is mandatory"),
+  body("state").notEmpty().withMessage("State is mandatory"),
+  body("country").notEmpty().withMessage("Country is mandatory"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Price must be greater than zero"),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("Estimated delivery time must be greater than zero"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines should be an array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array should not be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu item name is mandatory"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu item price is mandatory and should be greater than zero"),
+  handleValidationErrors,
+];
