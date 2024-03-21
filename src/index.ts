4e34ee0ea072a1de3d41712423e2548cab6c4570
bwesterg <benton.westergaard@gmail.com>
2024-03-21 13:03:21 -0600

    adds payment flow routes for Stripe and tests functionality with test credit card.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import myUserRoute from './routes/MyUserRoute';
 import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from './routes/MyRestaurantRoute';
 import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -34,6 +35,7 @@ app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
 // ^^^ user does NOT need to be logged in to use search
+app.use("/api/order", orderRoute);
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
